efbe40e66c1787b37a37b5baae95a7c07195aa76
Norio <redtree.dev1112@gmail.com>
2014-07-16 08:19:36 -0700

    Add more assets

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -18,10 +18,6 @@ bool HelloWorld::init()
         return false;
     }
     
-    Size visibleSize = Director::getInstance()->getVisibleSize();
-    Vec2 origin = Director::getInstance()->getVisibleOrigin();
-
-    
     return true;
 }
 
