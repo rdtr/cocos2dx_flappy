37c48879ce9408113ebd497344ca3a027e62d33e
Norio <redtree.dev1112@gmail.com>
2014-07-17 05:38:57 -0700

    Setup basic sprites

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -18,6 +18,22 @@ bool HelloWorld::init()
         return false;
     }
     
+    Size visibleSize = Director::getInstance()->getVisibleSize();
+    
+    // Ground setup
+    Sprite *groundSprite = Sprite::create("ground.png");
+    this->addChild(groundSprite);
+    groundSprite->setPosition(Vec2(308, 84)); //half of image size
+    
+    // SkyGround setup
+    Sprite *skySprite = Sprite::create("flappy_background.png");
+    this->addChild(skySprite);
+    skySprite->setPosition(visibleSize.width/2, 168 + 200);
+    
+    // bird setup
+    Sprite *birdSprite = Sprite::create("flappybird1.png");
+    this->addChild(birdSprite);
+    birdSprite->setPosition(visibleSize.width/2, visibleSize.height/2 + 120);
     return true;
 }
 
